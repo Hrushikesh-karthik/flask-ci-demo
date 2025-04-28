04fbb0476531e1856b95b5213b9a02e4b8ff1959
U Hrushikesh Karthik <143626493+Hrushikesh-karthik@users.noreply.github.com>
2025-04-28 18:08:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World! CI/CD working perfectly!"
+    return "Hello, World! CI/CD working perfectly! yeahh"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
